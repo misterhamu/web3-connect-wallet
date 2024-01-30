00f1d50c847bb8e957309ede4b479fd01bf48424
Thitipat Na Nakorn <thitipat.n@arise.tech>
2024-01-30 17:44:58 +0700

    chore add signmessage

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,33 @@
-import { ConnectButton } from '@rainbow-me/rainbowkit';
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { useSignMessage } from "wagmi";
 
 function Page() {
+  const { data, error, isLoading, signMessage, isError } = useSignMessage({
+    onSuccess(data, variables) {
+      alert(data);
+    },
+    onError(error) {},
+  });
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'flex-end',
-        padding: 12,
-      }}
-    >
-      <ConnectButton />
-    </div>
+    <>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "flex-end",
+          padding: 12,
+        }}
+      >
+        <ConnectButton />
+      </div>
+
+      <div
+        onClick={() => {
+          signMessage({ message: "test Sign Message" });
+        }}
+      >
+        Sign Message
+      </div>
+    </>
   );
 }
 
